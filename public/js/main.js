02dafe7c6c4643d5ec80fa1236c4bb4dea8da5a5
Friedrich Endlicher <fe@isarstyle.com>
2017-06-12 02:32:43 -0300

    working on user feedback

--- public/js/main.js
+++ public/js/main.js
@@ -2,6 +2,16 @@ $(document).ready(function() {
     // Initialize variables
     var $window = $(window);
     var socket = io();
+    socket.on('response', function (errorMsg) {
+        console.log(errorMsg);
+        swal({
+          type: 'success',
+          title: 'IOTA successfully sent!',
+          text: errorMsg,
+          html: 'Check your Wallet'
+        })
+
+    })
     //console.log("Document Ready");
     //Attach Eventlistener
     $( "#sendIotaBtn" ).click(function() {
@@ -13,37 +23,28 @@ $(document).ready(function() {
             return
         }
         //Add Response for Iota is Sending
-        swal({
-      title: 'Please confirm your address ! ' + address + ' this may take up to 10 minutes!',
+    swal({
+      type: 'question',
+      title: 'Please confirm your address!',
+      text: address + ' this may take up to 10 minutes!',
       confirmButtonText: 'Confirm Address',
       showLoaderOnConfirm: true,
       preConfirm: function () {
-        return new Promise(function (resolve, reject) {
-            // Socket events
-            socket.emit('send', getDataFromForm());
-            socket.on('response', function (errorMsg) {
-                console.log(errorMsg);
-                if (errorMsg === 'Successfully sent 1 IOTA') {
-                    resolve()
-                } else {
-                    reject(errorMsg)
-                }
-            });
-        })
+          // Send event
+          socket.emit('send', getDataFromForm());
+          //just wait a minute
+          return new Promise(function (resolve) {
+              setTimeout(function() {
+                  resolve()
+              }, 60000)
+          })
       },
       allowOutsideClick: false
-    }).then(function () {
-      swal({
-        type: 'success',
-        title: 'IOTA successfully sent!',
-        html: 'Check your Wallet'
-      })
     })
-
     });
     // Whenever the server emits 'balance', show the actual balance
       socket.on('balance', function (balance) {
-          console.log(balance);
+        //   console.log(balance);
           // Update total balance
           $("#iota__balance").html(balance.toString());
       });
